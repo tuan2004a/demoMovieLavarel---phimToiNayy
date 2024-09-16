c5791664d59526495e1e8fb17dff0d1203d1bb54
tuan2004a <bi03112004@gmail.com>
2024-09-16 20:10:46 +0700

    page Login & page Sign In

---
+++ resources/views/SignUp.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link
+      rel="stylesheet"
+      data-purpose="Layout StyleSheet"
+      title="Web Awesome"
+      href="/css/app-wa-462758aa1e172f82d39e1ea35e919e0a.css?vsn=d"
+    >
+
+      <link
+        rel="stylesheet"
+        href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css"
+      >
+
+      <link
+        rel="stylesheet"
+        href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-duotone-solid.css"
+      >
+
+      <link
+        rel="stylesheet"
+        href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-thin.css"
+      >
+
+      <link
+        rel="stylesheet"
+        href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-solid.css"
+      >
+
+      <link
+        rel="stylesheet"
+        href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-regular.css"
+      >
+
+      <link
+        rel="stylesheet"
+        href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-light.css"
+      >
+</head>
+<body>
+    <div class="min-h-[100vh] min-w-[100vw] bg-[#0d0e10] flex items-center justify-center">
+        <div class="w-[460px] bg-[#171717] rounded-2xl border border-gray-700 p-7">
+            <div class="flex justify-between items-center text-white">
+                <h1 class="text-xl text-white font-bold">Sign Up</h1>
+                <button class="text-lg size-10 rounded-xl border border-gray-800">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
+            </div>
+            <div class="w-full flex flex-col mt-8">
+                <label for="" class="mb-1 font-medium text-zinc-300">Tên người dùng</label>
+                <input type="text" class="outline-none bg-transparent p-2.5 text-sm bg-zinc-950 text-white rounded-lg placeholder-zinc-700" placeholder="Email">
+            </div>
+            <div class="w-full flex flex-col mt-3">
+                <label for="" class="mb-1 font-medium text-zinc-300">Email</label>
+                <input type="text" class="outline-none bg-transparent p-2.5 text-sm bg-zinc-950 text-white rounded-lg placeholder-zinc-700" placeholder="Email">
+            </div>
+            <div class="w-full flex flex-col mt-3">
+                <label for="" class="mb-1 font-medium text-zinc-300">Mật khẩu</label>
+                <input type="password" class="outline-none bg-transparent p-2.5 text-sm bg-zinc-950 text-white rounded-lg placeholder-zinc-700"  placeholder="Mật khẩu">
+            </div>
+            <div class="w-full flex flex-col mt-3">
+                <label for="" class="mb-1 font-medium text-zinc-300">Nhập mật khẩu</label>
+                <input type="password" class="outline-none bg-transparent p-2.5 text-sm bg-zinc-950 text-white rounded-lg placeholder-zinc-700"  placeholder="Mật khẩu">
+            </div>
+            <div class="flex items-center gap-x-1.5 my-3 cursor-pointer">
+                <input type="checkbox" id="agree" class="outline-none size-4 rounded-full accent-green-500 ">
+                <label for="agree" class="text-zinc-500 text-sm cursor-pointer">Tôi đồng ý với <strong class="text-zinc-200 mr-1">điều khoản</strong>và<strong class="ml-1 text-zinc-200">chính sách</strong></label>
+            </div>
+            <button class="py-2.5 w-full bg-gray-300 font-semibold transition-all duration-300 rounded-lg hover:bg-green-500 hover:text-white">
+                Đăng ký
+            </button>
+            <div class="text-zinc-500 text-sm font-medium text-center mt-4">
+                Đã có tài khoản?<a href="/dang-nhap" class="ml-1.5 font-bold text-zinc-200">Đăng nhập</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Laptop Asus Gaming ROG Strix G15 G513IH R7 (HN015W) - Chính hãng, trả góp</title>
+        <meta name="keywords" content="Asus Gaming ROG Strix G15 G513IH R7 4800H/8GB/512GB/4GB GTX1650/144Hz/Win11 (HN015W), Asus Gaming ROG Strix G15 G513IH R7 4800H (HN015W), Asus Gaming ROG Strix G15 G513IH R7 4800H (HN015W), Laptop Asus Gaming ROG Strix G15 G513IH R7 4800H/8GB/512GB/4GB GTX1650/144Hz/Win11 (HN015W), giá Asus Gaming ROG Strix G15 G513IH R7 4800H/8GB/512GB/4GB GTX1650/144Hz/Win11 (HN015W), thông tin Asus Gaming ROG Strix G15 G513IH R7 4800H/8GB/512GB/4GB GTX1650/144Hz/Win11 (HN015W)" />
+        <meta name="description" content="Laptop Asus Gaming ROG Strix G15 G513IH R7 4800H (HN015W) giá rẻ, trả góp - Mua online, xét duyệt nhanh, giao hàng tận nơi trong 1 giờ, cà thẻ tại nhà. Bảo hành toàn quốc. Xem ngay!" />
+        <meta property="og:url" itemprop="url" content="https://www.thegioididong.com/laptop/asus-rog-strix-gaming-g513ih-r7-hn015w" />
+        <meta property="og:title" content="Laptop Asus Gaming ROG Strix G15 G513IH R7 (HN015W) - Chính hãng, trả góp" />
+        <meta property="og:description" content="Laptop Asus Gaming ROG Strix G15 G513IH R7 4800H (HN015W) giá rẻ, trả góp - Mua online, xét duyệt nhanh, giao hàng tận nơi trong 1 giờ, cà thẻ tại nhà. Bảo hành toàn quốc. Xem ngay!" />
+        <meta property="og:image" content="https://img.tgdd.vn/1/wp2T87oZHtI-/16092024" />
+        <link rel="canonical" href="https://www.thegioididong.com/laptop/asus-rog-strix-gaming-g513ih-r7-hn015w" />
+        <meta name="RATING" content="GENERAL" />
+        <meta content="INDEX,FOLLOW" name="robots" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="copyright" content="Công ty Cổ phần Thế Giới Di Động" />
+        <meta name="author" content="Công ty Cổ phần Thế Giới Di Động" />
+        <meta http-equiv="audience" content="General" />
+        <meta name="resource-type" content="Document" />
+        <meta name="distribution" content="Global" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="GENERATOR" content="Công ty Cổ phần Thế Giới Di Động" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <link href="/favicon_TGDD.ico" rel="shortcut icon" type="image/x-icon" />
+        <link href="/favicon_TGDD.ico" rel="apple-touch-icon" />
+        <link href="/favicon_TGDD.ico" rel="apple-touch-icon-precomposed" />
+        <meta property="og:site_name" content="Thegioididong.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="vi_VN" />
+        <meta property="fb:pages" content="214993791879039" />
+        <meta http-equiv="x-dns-prefetch-control" content="on">
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+
+    <body class="antialiased min-h-[100vh] min-w-[100vw] flex flex-col items-center justify-center">
+        <h1 class="text-5xl mb-20 font-semibold">Hello word</h1>
+        <button class="inline-flex items-center justify-center p-0.5 overflow-hidden text-sm  text-gray-900 rounded-lg bg-gradient-to-br from-sky-500 to-blue-600">
+            <a href="/dang-nhap" class="px-5 py-2.5 transition-all ease-in duration-300 bg-white rounded-md font-semibold hover:text-white hover:bg-opacity-0">
+                Đăng nhập
+            </a>
+        </button>
+    </body>
+</html>
